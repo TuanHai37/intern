6735fd714c913b9bea801404ef9c794d0dfad58f
TuanHai37 <tuanhaicr69@gmail.com>
2021-04-06 15:26:51 +0700

    test file mocha

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,7 +4,6 @@ import express from 'express'
 let router =express.Router();
 
 let initRoutes=(app)=>{
-    
     return app.use("/",router);                                                    
 };
 module.exports=initRoutes;
