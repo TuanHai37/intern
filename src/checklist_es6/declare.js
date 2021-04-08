4a647e5ad43e24c22e24d26a95e4b48d559b53a1
TuanHai37 <tuanhaicr69@gmail.com>
2021-04-08 14:22:07 +0700

    move declare.js to Folder checklist_es6

---
+++ src/checklist_es6/declare.js
@@ -0,0 +1,32 @@
+  /**
+   * Var la function scope 
+   * Let, Const la block scope 
+   * Function scope bi gioi han trong function , block scope gioi han trong cap ngoac {} cua for,if...
+   */
+function myFunc() {  
+    var name = 'Luke'
+    console.log(name); // 'Luke'
+  }
+  
+  myFunc();
+  console.log(name); // name is not defined  
+
+  if(true) {  
+    var name = 'Luke'
+  }
+  
+  console.log(name); // 'Luke' 
+
+  let name = 'Luke';
+
+  if (true) {  
+    let name = 'Phil';
+    console.log(name); // 'Phil'
+  }
+  
+  console.log(name); // 'Luke'  
+
+  /**
+   * Var la function scope 
+   * Let, Const la block scope 
+   */
---
+++ src/checklist_es6/stringInterpolation.js
@@ -0,0 +1,10 @@
+
+let a="No"
+let x = `This is a new template literal`;
+
+console.log(`I'm a "Supper man" :${a}`);  // I'm a "Supper man" : No 
+
+let a = 5;
+let b = 10;
+console.log(`The sum of ${a} and ${b} is ${a+b}`);
+// Them sum of 5 and 10 is 15
