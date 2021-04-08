ca11a49a69408caa13b085b8c46d1f4a63a9a1d1
TuanHai37 <tuanhaicr69@gmail.com>
2021-04-08 14:43:42 +0700

    Multi-line strings -Checklist_ES6

---
+++ src/checklist_es6/multilineString.js
@@ -0,0 +1,18 @@
+const person = {
+    firstName: 'Le Quoc Tuan',
+    lastName: 'Hai',
+    age: 20,
+    city: 'Da Nang'
+}
+
+let info = `Name:   ${person.firstName} ${person.lastName}
+Age:    ${person.age}
+City:   ${person.city}`;
+
+console.log(info);
+
+/**
+ *  Name:   Le Quoc Tuan Hai
+    Age:    20
+    City:   Da Nang
+ */
