beed4fbe34c4b632854ba95c5812a77fd02855db
TuanHai37 <tuanhaicr69@gmail.com>
2021-04-08 14:38:06 +0700

    String interpolation - ChecklistES6

---
+++ src/checklist_es6/stringInterpolation.js
@@ -0,0 +1,11 @@
+let x = `This is a new template literal`;
+// This is a new template literal
+
+let a="No"
+console.log(`I'm a "Supper man":${a}`);
+// I'm a "Supper man":No
+
+let a=5
+let b=10
+console.log(`Tong cua ${a} va ${b} la ${a+b}`);
+// console.log(Tong cua 5 va 10 la 15);
